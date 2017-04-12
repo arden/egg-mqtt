432c45199d54f73ef1e9a3ee70df781ee5562ffd
arden <arden@tujiao.com>
2017-04-12 18:37:30 +0800

    add config files

--- test/fixtures/apps/mqtt-test/config/config.unittest.js
+++ test/fixtures/apps/mqtt-test/config/config.unittest.js
@@ -1,3 +1,17 @@
 'use strict';
 
+exports.mqtt = {
+  client: {
+    host: '127.0.0.1',
+    port: 7000,
+    user: 'arden',
+    password: '123456',
+    clientId: '0123456789',
+    connectTimeout: 30 * 1000,
+    keepalive: 5,
+    clean: false,
+  },
+  agent:true
+};
+
 exports.keys = '123456';
